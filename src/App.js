c4037a5e8f987edc10ee8f545fd87351b2b371ee
amsatoug <hamza98.hg@gmail.com>
2021-10-10 22:34:25 +0200

    changes for second test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from the official website
+          Learn React from the official website - second modif
         </a>
       </header>
     </div>
